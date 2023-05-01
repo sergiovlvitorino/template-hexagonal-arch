1b6a173bdeea65c8be218e43ad1b732e8e6babbe
Sergio Vitorino <sergiovlvitorino@gmail.com>
2023-05-01 10:47:37 -0300

    adicionando visitor para criação do ID

---
+++ src/main/kotlin/br/com/vitorino/templatehexagonalarch/person/visitor/PersonGenerateIdVisitor.kt
@@ -0,0 +1,19 @@
+package br.com.vitorino.templatehexagonalarch.person.visitor
+
+import br.com.vitorino.templatehexagonalarch.infrastructure.visitor.Visitor
+import br.com.vitorino.templatehexagonalarch.person.model.Person
+import org.slf4j.LoggerFactory
+import org.springframework.stereotype.Component
+import java.util.UUID
+
+@Component
+class PersonGenerateIdVisitor:Visitor<Person> {
+
+    private val log = LoggerFactory.getLogger(this::class.java)
+
+    override fun visit(person: Person) {
+        val id = UUID.randomUUID().toString()
+        log.info("Adicionando id {} para person {}", id, person)
+        person.id = id
+    }
+}
